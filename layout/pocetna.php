7611ba0ec345a890408dbbfb5f20f78de260aed7
TemovP <petar-temov@live.com>
2015-03-24 18:57:03 +0100

    smeneto

--- layout/pocetna.php
+++ layout/pocetna.php
@@ -8,7 +8,7 @@
 
 <div class="row">
 	
-		<div class="col-md-12"> 			
+		<div class="col-sm-12"> 			
 		
 		
 					<div class="carousel fade-carousel slide" data-ride="carousel" data-interval="2500" id="bs-carousel">
@@ -71,7 +71,7 @@
 
 
 		</div>
-		<div class="col-md-2"> 			</div>
+		<div class="col-sm-2"> 			</div>
 	</div>
 
 
