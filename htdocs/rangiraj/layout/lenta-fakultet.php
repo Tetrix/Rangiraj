94d1557767443f633978810ef729c4984c0fc01e
shakle17 <saklevaleksandar@gmail.com>
2015-11-17 21:30:38 +0100

    Fixed pages:zanas,pocetna,header,register,login.rangiraj,prof

---
+++ htdocs/rangiraj/layout/lenta-fakultet.php
@@ -0,0 +1,105 @@
+<?php 
+if(isset($_POST["ime_fax"]))
+{
+$name_fax=$_POST["ime_fax"];
+
+?>
+<div class="main2">
+<br>
+	<div class="row">
+			<div class="col-xs-2"></div>
+			
+			<div class="col-xs-8">
+
+				<h2 class="rezultaticol2"> <?php echo $name_fax ;  }?></h2>
+
+			</div>
+			
+			<div class="col-xs-2"></div>
+	</div>
+	
+	<div class="row">
+	<div class="col-xs-1"></div>
+			<div class="col-xs-2">
+				<h5  class="rezultaticol">Пракса
+				<h5>
+			</div>
+		<div class="col-xs-2"></div>
+			<div class="col-xs-2">
+				<h5  class="rezultaticol">Кадар<h5>
+			</div>
+				<div class="col-xs-2"></div>
+			<div class="col-xs-2">
+				<h5 class="rezultaticol">Услови<h5>
+			</div>
+			<div class="col-xs-1"></div>
+	</div>
+	<div class="row">
+			<div class="col-xs-1"></div>
+			<div class="col-xs-2">
+				<h5  class="rezultaticol"><!--ocena -->    &nbsp&nbsp<img src="pictures/zvez.png" height="25px" width="25px"><h5>
+			</div>
+				<div class="col-xs-2"></div>
+			<div class="col-xs-2">
+				<h5  class="rezultaticol">3.4  &nbsp&nbsp<img src="pictures/zvez.png" height="25px" width="25px"><h5>
+			</div>
+				<div class="col-xs-2"></div>
+			<div class="col-xs-2">
+				<h5  class="rezultaticol">5.0  &nbsp&nbsp<img src="pictures/zvez.png" height="25px" width="25px"><h5>
+			</div>
+			<div class="col-xs-1"></div>
+	</div>
+	<hr>
+	
+	
+	
+	
+	
+		<div class="row">
+			
+			
+			<div class="col-xs-6">
+			
+			<div class="row">
+			
+				<div class="col-xs-2">
+				<h5  class="rezultaticol1">Пракса<h5>
+				</div>
+				
+				<div class="col-xs-2"> </div>
+				
+				<div class="col-xs-2">
+				<h5  class="rezultaticol1">Кадар<h5>
+				</div>
+			
+				<div class="col-xs-2"> </div>
+			
+				<div class="col-xs-2">
+				<h5  class="rezultaticol1">Услови<h5>
+				</div>
+			
+			</div>
+			
+			</div>
+			
+	
+			<div class="col-xs-4">
+			
+					<textarea disabled id="resizeboxdisable" rows="3" cols="62"> Некој коментар за колку ни е убав серверо у факултето или нешо там </textarea>
+				
+			</div>
+			
+			<div class="col-xs-2">
+			
+			</div>
+
+			
+		
+	</div>
+	<hr>
+
+	</div>
+	</div>
+	
+	
+	
