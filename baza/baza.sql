bfa950ea4421c4cc5f523dbd0536bb510f77314c
Tetrix <deniporjazovski@yahoo.com>
2015-11-23 15:16:35 +0100

    Updated database

--- baza/baza.sql
+++ baza/baza.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 18, 2015 at 01:55 
--- Server version: 5.6.14
--- PHP Version: 5.5.6
+-- Generation Time: Nov 23, 2015 at 03:15 PM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `baza`
@@ -26,24 +26,26 @@ SET time_zone = "+00:00";
 -- Table structure for table `fakulteti`
 --
 
-CREATE TABLE IF NOT EXISTS `fakulteti` (
-  `id_fakultet` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fakulteti` (
+  `id_fakultet` int(11) NOT NULL,
   `ime_fakultet` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `rejting_na_fakultet` float NOT NULL,
-  PRIMARY KEY (`id_fakultet`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+  `prosek_praksa` float NOT NULL,
+  `prosek_kadar` float NOT NULL,
+  `prosek_uslovi` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `fakulteti`
 --
 
-INSERT INTO `fakulteti` (`id_fakultet`, `ime_fakultet`, `rejting_na_fakultet`) VALUES
-(8, 'Факултет за Информатика', 3.51282),
-(9, 'Електротехнички факултет', 0),
-(10, 'Факултет за медицински науки', 0),
-(11, 'Економски факултет', 0),
-(12, 'Правен факултет', 0),
-(13, 'Филолошки факултет', 0);
+INSERT INTO `fakulteti` (`id_fakultet`, `ime_fakultet`, `rejting_na_fakultet`, `prosek_praksa`, `prosek_kadar`, `prosek_uslovi`) VALUES
+(8, 'Факултет за Информатика', 3.51282, 0, 0, 0),
+(9, 'Електротехнички факултет', 0, 0, 0, 0),
+(10, 'Факултет за медицински науки', 0, 0, 0, 0),
+(11, 'Економски факултет', 0, 0, 0, 0),
+(12, 'Правен факултет', 0, 0, 0, 0),
+(13, 'Филолошки факултет', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -51,14 +53,11 @@ INSERT INTO `fakulteti` (`id_fakultet`, `ime_fakultet`, `rejting_na_fakultet`) V
 -- Table structure for table `fakultet_kampus`
 --
 
-CREATE TABLE IF NOT EXISTS `fakultet_kampus` (
-  `id_fak_kam` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fakultet_kampus` (
+  `id_fak_kam` int(11) NOT NULL,
   `id_fakultet` int(11) NOT NULL,
-  `id_kampus` int(11) NOT NULL,
-  PRIMARY KEY (`id_fak_kam`),
-  UNIQUE KEY `id_fakultet` (`id_fakultet`),
-  KEY `id_kampus` (`id_kampus`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=13 ;
+  `id_kampus` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `fakultet_kampus`
@@ -78,14 +77,11 @@ INSERT INTO `fakultet_kampus` (`id_fak_kam`, `id_fakultet`, `id_kampus`) VALUES
 -- Table structure for table `fakultet_profesor`
 --
 
-CREATE TABLE IF NOT EXISTS `fakultet_profesor` (
-  `id_fak_prof` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fakultet_profesor` (
+  `id_fak_prof` int(11) NOT NULL,
   `id_fakultet` int(11) NOT NULL,
-  `id_profesor` int(11) NOT NULL,
-  PRIMARY KEY (`id_fak_prof`),
-  KEY `id_profesor` (`id_profesor`),
-  KEY `id_fakultet` (`id_fakultet`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  `id_profesor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `fakultet_profesor`
@@ -104,18 +100,15 @@ INSERT INTO `fakultet_profesor` (`id_fak_prof`, `id_fakultet`, `id_profesor`) VA
 -- Table structure for table `fakultet_rang`
 --
 
-CREATE TABLE IF NOT EXISTS `fakultet_rang` (
-  `id_fak_rang` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `fakultet_rang` (
+  `id_fak_rang` int(11) NOT NULL,
   `id_fakultet` int(11) NOT NULL,
   `id_korisnik` int(11) NOT NULL,
   `praksa` float NOT NULL,
   `kadar` float NOT NULL,
   `uslovi` float NOT NULL,
-  `fax_kom` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id_fak_rang`),
-  KEY `id_fakultet` (`id_fakultet`),
-  KEY `id_korisnik` (`id_korisnik`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=90 ;
+  `fax_kom` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `fakultet_rang`
@@ -142,21 +135,23 @@ INSERT INTO `fakultet_rang` (`id_fak_rang`, `id_fakultet`, `id_korisnik`, `praks
 -- Table structure for table `kampus`
 --
 
-CREATE TABLE IF NOT EXISTS `kampus` (
-  `id_kampus` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `kampus` (
+  `id_kampus` int(11) NOT NULL,
   `ime_kampus` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `rejting_na_kampus` float NOT NULL,
-  PRIMARY KEY (`id_kampus`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+  `prosek_higiena` float NOT NULL,
+  `prosek_lokacija` float NOT NULL,
+  `prosek_uslovi` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kampus`
 --
 
-INSERT INTO `kampus` (`id_kampus`, `ime_kampus`, `rejting_na_kampus`) VALUES
-(4, 'Кампус 2', 3.25),
-(5, 'Кампус 3', 4.66667),
-(6, 'Кампус 4', 0);
+INSERT INTO `kampus` (`id_kampus`, `ime_kampus`, `rejting_na_kampus`, `prosek_higiena`, `prosek_lokacija`, `prosek_uslovi`) VALUES
+(4, 'Кампус 2', 3.25, 0, 0, 0),
+(5, 'Кампус 3', 4.66667, 0, 0, 0),
+(6, 'Кампус 4', 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -164,14 +159,11 @@ INSERT INTO `kampus` (`id_kampus`, `ime_kampus`, `rejting_na_kampus`) VALUES
 -- Table structure for table `kampus_profesor`
 --
 
-CREATE TABLE IF NOT EXISTS `kampus_profesor` (
-  `id_kam_prof` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `kampus_profesor` (
+  `id_kam_prof` int(11) NOT NULL,
   `id_kampus` int(11) NOT NULL,
-  `id_profesor` int(11) NOT NULL,
-  PRIMARY KEY (`id_kam_prof`),
-  KEY `id_kampus` (`id_kampus`),
-  KEY `id_profesor` (`id_profesor`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
+  `id_profesor` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kampus_profesor`
@@ -190,18 +182,15 @@ INSERT INTO `kampus_profesor` (`id_kam_prof`, `id_kampus`, `id_profesor`) VALUES
 -- Table structure for table `kampus_rang`
 --
 
-CREATE TABLE IF NOT EXISTS `kampus_rang` (
-  `id_kam_rang` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `kampus_rang` (
+  `id_kam_rang` int(11) NOT NULL,
   `id_kampus` int(11) NOT NULL,
   `id_korisnik` int(11) NOT NULL,
   `higiena` float NOT NULL,
   `lokacija` float NOT NULL,
   `uslovi` float NOT NULL,
-  `kam_komentar` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id_kam_rang`),
-  KEY `id_kampus` (`id_kampus`),
-  KEY `id_korisnik_2` (`id_korisnik`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=25 ;
+  `kam_komentar` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kampus_rang`
@@ -218,12 +207,11 @@ INSERT INTO `kampus_rang` (`id_kam_rang`, `id_kampus`, `id_korisnik`, `higiena`,
 -- Table structure for table `korisnici`
 --
 
-CREATE TABLE IF NOT EXISTS `korisnici` (
-  `id_korisnik` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `korisnici` (
+  `id_korisnik` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  PRIMARY KEY (`id_korisnik`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `korisnici`
@@ -238,23 +226,26 @@ INSERT INTO `korisnici` (`id_korisnik`, `username`, `password`) VALUES
 -- Table structure for table `profesori`
 --
 
-CREATE TABLE IF NOT EXISTS `profesori` (
-  `id_profesor` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `profesori` (
+  `id_profesor` int(11) NOT NULL,
   `ime_prezime_prof` varchar(25) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `rejting_na_profesori` float NOT NULL,
-  PRIMARY KEY (`id_profesor`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
+  `prosek_odgovornost` float NOT NULL,
+  `prosek_predavanja` float NOT NULL,
+  `prosek_literatura` float NOT NULL,
+  `prof_img` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `profesori`
 --
 
-INSERT INTO `profesori` (`id_profesor`, `ime_prezime_prof`, `rejting_na_profesori`) VALUES
-(39, 'Сашо Коцевски', 1.48438),
-(40, 'Цвета Мартиновска', 0),
-(41, 'Зоран Утковски', 0),
-(42, 'Игор Стојановиќ', 0),
-(43, 'Благој Делипетрев', 0);
+INSERT INTO `profesori` (`id_profesor`, `ime_prezime_prof`, `rejting_na_profesori`, `prosek_odgovornost`, `prosek_predavanja`, `prosek_literatura`, `prof_img`) VALUES
+(39, 'Сашо Коцевски', 1.48438, 0, 0, 0, ''),
+(40, 'Цвета Мартиновска', 0, 0, 0, 0, ''),
+(41, 'Зоран Утковски', 0, 0, 0, 0, ''),
+(42, 'Игор Стојановиќ', 0, 0, 0, 0, ''),
+(43, 'Благој Делипетрев', 0, 0, 0, 0, '');
 
 -- --------------------------------------------------------
 
@@ -262,18 +253,15 @@ INSERT INTO `profesori` (`id_profesor`, `ime_prezime_prof`, `rejting_na_profesor
 -- Table structure for table `prof_rang`
 --
 
-CREATE TABLE IF NOT EXISTS `prof_rang` (
-  `id_prof_rang` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `prof_rang` (
+  `id_prof_rang` int(11) NOT NULL,
   `id_prof` int(11) NOT NULL,
   `id_korisnik` int(11) NOT NULL,
   `odgovornost` float NOT NULL,
   `predavanja` float NOT NULL,
   `literatura` float NOT NULL,
-  `prof_kom` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id_prof_rang`),
-  KEY `id_prof` (`id_prof`),
-  KEY `id_korisnik` (`id_korisnik`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+  `prof_kom` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `prof_rang`
@@ -283,6 +271,136 @@ INSERT INTO `prof_rang` (`id_prof_rang`, `id_prof`, `id_korisnik`, `odgovornost`
 (11, 39, 1, 5, 5, 5, 'aaaa'),
 (19, 39, 1, 1, 1, 1, 'agagagaga');
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `fakulteti`
+--
+ALTER TABLE `fakulteti`
+  ADD PRIMARY KEY (`id_fakultet`);
+
+--
+-- Indexes for table `fakultet_kampus`
+--
+ALTER TABLE `fakultet_kampus`
+  ADD PRIMARY KEY (`id_fak_kam`),
+  ADD UNIQUE KEY `id_fakultet` (`id_fakultet`),
+  ADD KEY `id_kampus` (`id_kampus`);
+
+--
+-- Indexes for table `fakultet_profesor`
+--
+ALTER TABLE `fakultet_profesor`
+  ADD PRIMARY KEY (`id_fak_prof`),
+  ADD KEY `id_profesor` (`id_profesor`),
+  ADD KEY `id_fakultet` (`id_fakultet`);
+
+--
+-- Indexes for table `fakultet_rang`
+--
+ALTER TABLE `fakultet_rang`
+  ADD PRIMARY KEY (`id_fak_rang`),
+  ADD KEY `id_fakultet` (`id_fakultet`),
+  ADD KEY `id_korisnik` (`id_korisnik`);
+
+--
+-- Indexes for table `kampus`
+--
+ALTER TABLE `kampus`
+  ADD PRIMARY KEY (`id_kampus`);
+
+--
+-- Indexes for table `kampus_profesor`
+--
+ALTER TABLE `kampus_profesor`
+  ADD PRIMARY KEY (`id_kam_prof`),
+  ADD KEY `id_kampus` (`id_kampus`),
+  ADD KEY `id_profesor` (`id_profesor`);
+
+--
+-- Indexes for table `kampus_rang`
+--
+ALTER TABLE `kampus_rang`
+  ADD PRIMARY KEY (`id_kam_rang`),
+  ADD KEY `id_kampus` (`id_kampus`),
+  ADD KEY `id_korisnik_2` (`id_korisnik`);
+
+--
+-- Indexes for table `korisnici`
+--
+ALTER TABLE `korisnici`
+  ADD PRIMARY KEY (`id_korisnik`);
+
+--
+-- Indexes for table `profesori`
+--
+ALTER TABLE `profesori`
+  ADD PRIMARY KEY (`id_profesor`);
+
+--
+-- Indexes for table `prof_rang`
+--
+ALTER TABLE `prof_rang`
+  ADD PRIMARY KEY (`id_prof_rang`),
+  ADD KEY `id_prof` (`id_prof`),
+  ADD KEY `id_korisnik` (`id_korisnik`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `fakulteti`
+--
+ALTER TABLE `fakulteti`
+  MODIFY `id_fakultet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
+-- AUTO_INCREMENT for table `fakultet_kampus`
+--
+ALTER TABLE `fakultet_kampus`
+  MODIFY `id_fak_kam` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `fakultet_profesor`
+--
+ALTER TABLE `fakultet_profesor`
+  MODIFY `id_fak_prof` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `fakultet_rang`
+--
+ALTER TABLE `fakultet_rang`
+  MODIFY `id_fak_rang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+--
+-- AUTO_INCREMENT for table `kampus`
+--
+ALTER TABLE `kampus`
+  MODIFY `id_kampus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT for table `kampus_profesor`
+--
+ALTER TABLE `kampus_profesor`
+  MODIFY `id_kam_prof` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT for table `kampus_rang`
+--
+ALTER TABLE `kampus_rang`
+  MODIFY `id_kam_rang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+--
+-- AUTO_INCREMENT for table `korisnici`
+--
+ALTER TABLE `korisnici`
+  MODIFY `id_korisnik` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `profesori`
+--
+ALTER TABLE `profesori`
+  MODIFY `id_profesor` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+--
+-- AUTO_INCREMENT for table `prof_rang`
+--
+ALTER TABLE `prof_rang`
+  MODIFY `id_prof_rang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- Constraints for dumped tables
 --
